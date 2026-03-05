9ffc82a2195b9568f3c76efa496c4d3f92db3548
faria200118 <fariapreeticse@gmail.com>
2026-03-05 20:44:57 +0600

    firstprogram.py

---
+++ FirstProgram.py
@@ -0,0 +1,70 @@
+name = "Faria"
+age = 25
+price = 25.99
+
+print("My name is : ",name)
+print("My age is : ",age)
+print(price)
+print(type(name))
+print(type(age))
+print(type(price))
+old = False
+a = None
+print(type(old))
+print(type(a))
+b = 5
+c = 2
+diff = b-c
+print(diff)
+# i am a comment
+"""multi-line
+comment"""
+#Arithmetic Operator
+print(b+c)
+print(b-c)
+print(b*c)
+print(b/c)
+print(b%c)
+print(b**c)
+
+#relational operator
+print(b==c)
+print(b!=c)
+print(b>=c)
+print(b>c)
+print(b<=c)
+print(b<c)
+
+#assignment operators
+num = 10
+num += 10
+print("num :",num)
+
+num = 10
+num -= 10
+print("num1 :",num)
+
+num = 10
+num *= 10
+print("num2 :",num)
+
+num = 10
+num /= 10
+print("num3 :",num)
+
+num = 10
+num %= 10
+print("num4 :",num)
+
+#Logical Operator
+
+d = 50
+e = 30
+print(not False)
+print(not (d > e))
+
+val1 = False
+val2 = False
+print("AND operator:", val1 and val2)
+
+print("OR operator:",val1 or val2)
